2e1276bf1fcffeef5d6c76e6bb53dd0c7dc3fb96
ExploHash <jbtwaalf@gmail.com>
2023-06-28 23:26:31 +0200

    feat: copy pasted some random example

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export CPATH=/opt/homebrew/include
+export LIBRARY_PATH=/opt/homebrew/lib
 cmake -DCMAKE_BUILD_TYPE=Debug .
 cmake --build . -- -j4
-./build/TelChat
+./build/GameOfThrive
